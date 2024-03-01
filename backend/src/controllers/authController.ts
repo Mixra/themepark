12635153d97f348604c24cec474b24d1cf83b444
Clown <46231841+Mixra@users.noreply.github.com>
2024-03-01 01:13:08 -0600

    setup backend directories and basic config

---
+++ backend/src/controllers/authController.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from 'express';
+import { sql } from '../database/db';
+
+
+interface User {
+  username: string;
+  password: string;
+}
+
+export const register = async (req: Request, res: Response) => {
+  // Waiting schema
+
+  
+  
+};
+
+export const login = async (req: Request, res: Response) => {
+  
+};
+
+export const logout = async (req: Request, res: Response) => {
+
+};
+
+
